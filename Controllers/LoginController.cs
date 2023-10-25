5da5023d131225e3466c4928b459d6d3ff7a9652
GariboGE <ericgaribog@gmail.com>
2023-10-24 18:07:07 -0600

    Complemento

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wave.Models;
+
+namespace Wave.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly WaveDbContext _context;
+
+        public LoginController(WaveDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp()
+        {
+            string username = Request.Form["Username"];
+            string email = Request.Form["Email"];
+            string password = Request.Form["Password"];
+
+            // Validar si existe, si no guardar y crear usuario y mostrar mensaje de usurio creado, si si, mandar mensaje de que ya existe una cuenta asociada a ese correo
+
+
+            return View(); // O redirigir a otra vista, etc.
+        }
+
+        [HttpPost]
+        public ActionResult SignIn()
+        {
+            string email = Request.Form["Email"];
+            string password = Request.Form["Password"];
+
+            // Validar si existe usuario, si si, redirigiir a la pagina del perfil
+            return View();
+
+            // si no enviar mensaje de error por que no existe ese usuario
+        }
+    }
+}
--- Models/WaveDbContext.cs
+++ Models/WaveDbContext.cs
@@ -20,8 +20,8 @@ public partial class WaveDbContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-U7UB4HFG\\SQLEXPRESS; Database=waveDB; Trusted_Connection=True; TrustServerCertificate=True");
+    #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
