8f25e1b310396624f2e3f06d26aedfc7ef0d2dd5
Bernardo Lira <harritolira@gmail.com>
2023-12-06 09:16:41 -0600

    Add like function

--- wwwroot/js/posts.js
+++ wwwroot/js/posts.js
@@ -1,39 +1,52 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const postItems = document.querySelectorAll(".post_popup-item");
-    const popupContainer = document.getElementById("post_popupContainer");
-    const popupImage = document.getElementById("post_popup-image");
-    const popupDescription = document.getElementById("post_popup-description");
-    const closePopupBtn = document.getElementById("post_popup-close");
+// document.addEventListener("DOMContentLoaded", function () {
+//     const postItems = document.querySelectorAll(".post_popup-item");
+//     const popupContainer = document.getElementById("post_popupContainer");
+//     const popupImage = document.getElementById("post_popup-image");
+//     const popupDescription = document.getElementById("post_popup-description");
+//     const closePopupBtn = document.getElementById("post_popup-close");
 
-    postItems.forEach((postItem) => {
-        postItem.addEventListener("click", function () {
-            const imageSrc = this.querySelector(".post_popup-image").src;
-            const description = this.getAttribute("data-description");
+//     postItems.forEach((postItem) => {
+//         postItem.addEventListener("click", function () {
+//             const imageSrc = this.querySelector(".post_popup-image").src;
+//             const description = this.getAttribute("data-description");
 
-            // Actualizar el contenido del popup
-            popupImage.src = imageSrc;
+//             // Actualizar el contenido del popup
+//             popupImage.src = imageSrc;
 
-            //Quitar estos comentarios cuando la base de datos este conectada
-            // popupTitle.textContent = title;
-            popupDescription.textContent = description;
+//             //Quitar estos comentarios cuando la base de datos este conectada
+//             // popupTitle.textContent = title;
+//             popupDescription.textContent = description;
 
-            // Mostrar el popup
-            popupContainer.style.display = "block";
-        });
-    });
+//             // Mostrar el popup
+//             popupContainer.style.display = "block";
+//         });
+//     });
 
-    closePopupBtn.addEventListener("click", function () {
-        // Cerrar el popup al hacer clic en el botón de cierre
-        popupContainer.style.display = "none";
-    });
+//     closePopupBtn.addEventListener("click", function () {
+//         // Cerrar el popup al hacer clic en el botón de cierre
+//         popupContainer.style.display = "none";
+//     });
 
-    // Cerrar el popup al hacer clic fuera de él
-    document.addEventListener("click", function (e) {
-        if (
-            !popupContainer.contains(e.target) &&
-            !Array.from(postItems).some((item) => item.contains(e.target))
-        ) {
-            popupContainer.style.display = "none";
-        }
-    });
-});
+//     // Cerrar el popup al hacer clic fuera de él
+//     document.addEventListener("click", function (e) {
+//         if (
+//             !popupContainer.contains(e.target) &&
+//             !Array.from(postItems).some((item) => item.contains(e.target))
+//         ) {
+//             popupContainer.style.display = "none";
+//         }
+//     });
+// });
+
+const imgs = document.getElementsByClassName("img_post");
+const icons = document.getElementsByClassName("heart");
+
+for (let i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener("dblclick", () => {
+    icons[i].classList.add("like");
+
+    setTimeout(() => {
+      icons[i].classList.remove("like");
+    }, 1000);
+  });
+}
